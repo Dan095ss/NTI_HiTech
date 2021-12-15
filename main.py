3cbd57ad691988244e3b2bd754cfd6708b31bfab
Daniil (Dan095ss) <dan095ss2005@yandex.ru>
2021-12-15 14:50:49 +1000

    temp intgr 1

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ temp = 0
 
 @app.get("/")
 async def root():
+    print(temp)
     return {"message": temp}
 
 
 @app.post("/get_data")
 async def get_data(data:dict):
     global temp
-    temp = data['data']['uplink_message']['decoded_payload']['temperature']
+    temp = data['uplink_message']['decoded_payload']
+    print(temp)
     return {"success": True}
