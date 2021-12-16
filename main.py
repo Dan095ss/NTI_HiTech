3922627c53f8c8b7faf7a39e30e1d588b0d4c672
Daniil (Dan095ss) <dan095ss2005@yandex.ru>
2021-12-16 13:01:27 +1000

    first normal ipdate

--- main.py
+++ main.py
@@ -1,18 +1,69 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-temp = 0
+
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
-@app.get("/")
-async def root():
-    print(temp)
-    return {"message": temp}
+
+# temperature = 0
+mdata = []
+labels = []
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/get_data")
-async def get_data(data:dict):
-    global temp
-    temp = data['uplink_message']['decoded_payload']
-    print(temp)
-    return {"success": True}
+async def get_data(data: dict):
+    global labels
+    global mdata
+    print(data)
+    temperature = data['uplink_message']['decoded_payload']['temperature']
+    time = data['received_at']
+    t = time.split('.')[0].split(':')[1:]
+    print(t)
+    labels.append(t[0] + ':' + t[1])
+    mdata.append(temperature)
+    print(labels)
+    print(mdata)
+    return {'success': True}
+
+
+@app.get("/get_data_chart")
+async def root():
+    global labels
+    global mdata
+    chart = {'labels': labels,
+             'datasets': [
+                 {
+                     'label': 'температура inside',
+                     'backgroundColor': '#5c5cff',
+                     'data': mdata
+                 }
+             ]
+             }
+    return {"chart": chart}
